171ca918131fec0855eee0addba32b6613445960
taehee1 <163071930+taehee1@users.noreply.github.com>
2024-03-12 11:17:47 +0900

    GiftBox 추가
    letter 와 money를 넣고 배송기사님도 넣어줬다

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace ConsoleApp1
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내지?",
+                Money = 100000
+            };
+
+            GiftBox 배송기사님 = a;
+                                   
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 150000;
+
         }
     }
 }
