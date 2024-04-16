be82e0679d610481fefa6cea9e03b1f13fc4ddc6
leechuanhung <163071930+taehee1@users.noreply.github.com>
2024-04-16 09:31:37 +0900

    value type

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,8 +7,24 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            GiftBoxManager manager = new GiftBoxManager();
-            manager.Setlnit();
+            Console.WriteLine("시작부문");
+
+            int aMoney = 10000;
+            int bMoney = aMoney;
+            aMoney = 20000;
+            Console.WriteLine("aMoney : " + aMoney);
+            Console.WriteLine("bMoney : " + bMoney);
+
+            GiftBox aGiftBox = new GiftBox()
+            {
+                Letter = "안녕",
+                Money = 10000
+            };
+
+            GiftBox bGiftBox = aGiftBox;
+            aGiftBox.Money = 20000;
+            Console.WriteLine("aGiftBox : " + aGiftBox.Money);
+            Console.WriteLine("bGiftBox : " + bGiftBox.Money);
         }
     }
 }
